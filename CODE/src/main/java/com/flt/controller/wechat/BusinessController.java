75b408740b09bf19bbbe90af72236bd858df6c25
lotte <15281060106@163.com>
2015-12-05 11:07:38 +0800

    update

--- CODE/src/main/java/com/flt/controller/wechat/BusinessController.java
+++ CODE/src/main/java/com/flt/controller/wechat/BusinessController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.flt.service.base.BusinessService;
 
@@ -16,10 +15,10 @@ public class BusinessController {
 	
 	@Autowired
 	private BusinessService businessService;
-
-	@ResponseBody
+	
 	@RequestMapping("page-login")
 	public String pageLogin(Model model,HttpServletRequest req){
-		return this.businessService.test();
+		model.addAttribute("testValue", this.businessService.test());
+		return "example.ftl";
 	}
 }
---
+++ CODE/src/main/webapp/WEB-INF/example.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>example</title>
+	</head>
+	<body>
+	<h1>${testValue}</h1>
+	</body>
+</html>
